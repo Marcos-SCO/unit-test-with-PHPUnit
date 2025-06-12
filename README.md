1907edc83707074a27886f0a992be6a796f4272c
marcos-sco <marcos_sco@outlook.com>
2025-06-12 17:01:58 -0300

    feat: test of functions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # unit-test-with-PHPUnit
+
+## PHP Unit Commands
+
+- Unit version
+```bash
+vendor/bin/phpunit --version  
+```
+
+- Run an example class
+ ```bash
+ vendor/bin/phpunit tests/ExampleTest.php --colors=always
+ ```
+
+- Test Of functions
+ ```bash
+ vendor/bin/phpunit tests/FunctionsTest.php --colors=always
+ ```
---
+++ tests/FunctionsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require dirname(__DIR__) . '/lib/functions.php';
+
+final class FunctionsTest extends TestCase
+{
+    public function testAddTwoPositiveIntegers(): void
+    {
+        $this->assertSame(6, addIntegers(2, 3));
+    }
+
+    public function testAddTwoNegativeIntegers(): void
+    {
+        $this->assertSame(-5, addIntegers(-2, -3));
+    }
+
+    public function testAddPositiveAndNegativeInteger(): void
+    {
+        $this->assertSame(1, addIntegers(3, -2));
+    }
+
+    public function testAddZeroToInteger(): void
+    {
+        $this->assertSame(1, addIntegers(1, 0));
+    }
+
+    public function testAddingIsCommulative(): void
+    {
+        $this->assertSame(addIntegers(3, 2), addIntegers(3, 2));
+    }
+}
