6f91644b5cdfda82cd72e460f73bac85ce7b9207
marcos-sco <marcos_sco@outlook.com>
2025-06-13 18:12:56 -0300

    feat: test data provider

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 final class FunctionsTest extends TestCase
 {
-    public function testAddTwoPositiveIntegers(): void
+    /*public function testAddTwoPositiveIntegers(): void
     {
         $this->assertSame(6, addIntegers(3, 3));
     }
@@ -24,6 +25,23 @@ final class FunctionsTest extends TestCase
     public function testAddZeroToInteger(): void
     {
         $this->assertSame(1, addIntegers(1, 0));
+    }*/
+
+    public static function additionProvider(): array
+    {
+        // first, second, expected
+        return [
+            "two positive integers" => [2, 3, 5],
+            "two negative integers" => [-2, -3, -5],
+            "positive and negative integers" => [3, -2, 1],
+            "adding zero" => [3, 0, 3],
+        ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testAddIntegers(int $a, int $b, int $expected): void
+    {
+        $this->assertSame($expected, addIntegers($a, $b));
     }
 
     public function testAddingIsCommulative(): void
